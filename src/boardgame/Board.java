4841f6fcfc640e971added08f720ce035ea3d344
Gabriel Veríssimo <vrssmgabriel@gmail.com>
2023-10-25 20:45:32 -0300

    Created classes bishop and Pawn. Implemented moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -27,7 +27,7 @@ public class Board {
 		if (!positionExists(row, column)) {
 			throw new BoardException("Position not on the board.");
 		}
-		
+
 		return pieces[row][column];
 	}
 
@@ -35,7 +35,7 @@ public class Board {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board.");
 		}
-		
+
 		return pieces[position.getRow()][position.getColumn()];
 	}
 
@@ -45,6 +45,23 @@ public class Board {
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board.");
+		}
+
+		if (piece(position) == null) {
+			return null;
+		}
+
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+
+		return aux;
+
 	}
 
 	public boolean positionExists(int row, int column) {
